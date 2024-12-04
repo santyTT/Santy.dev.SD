d00644a57db1f3d372c8ee1dfea39710a22d617d
Santy-Writer <128934991+santyTT@users.noreply.github.com>
2024-12-04 13:38:06 -0500

    WEB MENU

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded' = () => {
+const menuToggle = document.querySelector('.menu-toggle');
+const html = document.documentElement;
+const menu = document.querySelector('nav ul');
+const menuItems = document.querySelectorAll('.menu-animation');
+
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active'); 
+    html.classList.toggle('no-scroll');
+});
+
+
+menuItems.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const targetId = e.target.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth' }); 
+        }
+
+        menu.classList.remove('active');
+        html.classList.remove('no-scroll');
+    });
+});
+});
+
